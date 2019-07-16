7776c72e209d5b02ccc8bc9ef6e9573a7b889c19
Shaun Alexander <shaun@sierraalpha.co.nz>
2019-07-16 14:33:49 +1200

    linked list double access causing segmentation fault

--- SnipIt.cpp
+++ SnipIt.cpp
@@ -26,14 +26,17 @@ class List {
 		T data;
 		Node *next;
 	};
-	Node *front, *current;
+	Node *front; 
+    vector<Node *> current;
 
    public:
 	List();
 	~List();
 	void AddtoFront(T newthing);
-	bool FirstItem(T &item);
-	bool NextItem(T &item);
+    int newCurrent();
+	bool FirstItem(int thread, T &item);
+	bool NextItem(int thread, T &item);
+    void threadReset();
 };
 
 template <class T>
@@ -59,7 +62,7 @@ void List<T>::AddtoFront(T newthing) {
 }
 
 template <class T>
-bool List<T>::FirstItem(T &item) {
+bool List<T>::FirstItem(int thread, T &item) {
 	if (front == NULL) {
 		return false;
 	}
@@ -118,27 +121,32 @@ bool Sample::NextOnQueue(int i, int &value) {
 struct ReadInSamples {
     Sample *ReadInThisSample;
     istringstream ReadInLine[2];
-    char delim;
+    char *delim;
+};
+
+struct snipItStruct{
+    int *lowindex, *highindex;
+    Sample *workOnMe[2];
+    string output;
 };
 
 void *ReadInSampleFunc(void *sampleReadIn) {
-	ReadInSamples *readMeIn = (ReadInSamples *)sampleReadIn;
+	ReadInSamples *readMeIn = (ReadInSamples *)sampleReadIn;            // cast sampleReadIn from a void * back to a ReadInSamples
     string token;
 	for (int i = 0; i < 2; ++i) {
-            cout << "Make it here?" << endl;
-		getline( readMeIn -> ReadInLine[i], token, readMeIn -> delim);
+		getline( readMeIn -> ReadInLine[i], token, *(readMeIn -> delim));  // get first word of line, sample title
 		if (i % 2 == 0) {
-			readMeIn->ReadInThisSample->MakeTitle(token);
+			readMeIn -> ReadInThisSample -> MakeTitle(token);               // if its the first line adf this to the sample title, if not ignore it
 
 			// string temp1;                        // Debugging to see that names ae loading correctly
 			// AllSamples[idiv2].CopyName(&temp1);
 			// std::cout << temp1 << endl;
 		}
-		while (getline (readMeIn->ReadInLine[i], token, readMeIn->delim) ) {
+		while ( getline( readMeIn -> ReadInLine[i], token, *(readMeIn -> delim) ) ) {    // Tokenise the rest of the data
 			// cout << token << endl; // for debugging
-			if (token[0] == '-' || isdigit(token[0])) {
-				int mynumber = stoi(token);
-				readMeIn->ReadInThisSample->AddToAllele(i, mynumber);  // to add to the correct array of allele queues
+			if (token[0] == '-' || isdigit(token[0])) {                         // make sure its a valid number
+				int mynumber = stoi(token);                                     // string to integer it
+				readMeIn->ReadInThisSample->AddToAllele(i, mynumber);           // to add to the correct array of allele queues
 
         //         // int temp;
         //         // AllSamples[idiv2]->firstOnQueue((i%2), temp);   // Debugging code
@@ -148,7 +156,6 @@ void *ReadInSampleFunc(void *sampleReadIn) {
 		}
 	}
     delete readMeIn;
-    cout << "or here" << endl;
     return NULL;
 }
 
@@ -242,6 +249,100 @@ int CheckHetHetMisMatch(int value[2][2]) {
 	}
 }
 
+void * snipAndString(void * cutMeUp){
+    snipItStruct *sampleSnip = (snipItStruct *)cutMeUp;
+
+	int TotalSnps, mismatchedSnps, mismatchBothHoms, mismatchHetHomOverlap, mismatchHetHomNonOverlap, mismatchHetHet;
+	float mismatchedSnpsPerc, mismatchBothHomsPerc, mismatchHetHomOverlapPerc, mismatchHetHomNonOverlapPerc, mismatchHetHetPerc;
+
+	TotalSnps = 0;
+	mismatchedSnps = 0;
+	mismatchBothHoms = 0;
+	mismatchHetHomOverlap = 0;
+	mismatchHetHomNonOverlap = 0;
+	mismatchHetHet = 0;
+
+	int value[2][2];  // Load an array with the sample values for comparison
+	bool ok1, ok2, ok3, ok4;
+	ok1 = sampleSnip -> workOnMe[0] -> firstOnQueue(0, value[0][0]);  // i is the first sample name to compare, 0 is the 1st allele from that sample name
+	ok2 = sampleSnip -> workOnMe[0] -> firstOnQueue(1, value[0][1]);  // i is the first sample name to compare, 1 is the 2nd allele from that sample name
+	ok3 = sampleSnip -> workOnMe[1] -> firstOnQueue(0, value[1][0]);  // j is the second sample name to compare, 0 is the 1st allele from that sample name
+	ok4 = sampleSnip -> workOnMe[1] -> firstOnQueue(1, value[1][1]);  // j is the second sample name to compare, 1 is the 2nd allele from that sample name
+
+	// while (ok1 && ok2 && ok3 && ok4) {
+		sortSnips(value);
+
+		if (CheckTotal(value, *sampleSnip -> lowindex, *sampleSnip -> highindex)) {
+			TotalSnps++;
+			if (CheckMisMatch(value)) {
+				mismatchedSnps++;
+				if (CheckHomMisMatch(value)) {
+					mismatchBothHoms++;
+				} else if (CheckHetHomOverlapMismatch(value)) {
+					mismatchHetHomOverlap++;
+				} else if (CheckHetHomNonOverlapMismatch(value)) {
+					mismatchHetHomNonOverlap++;
+				} else if (CheckHetHetMisMatch(value)) {
+					mismatchHetHet++;
+				}
+			}
+		}
+
+		// std::cout << value[0][0] << value[0][1] << value[1][0] << value[1][1] << endl; // for debugging
+		// ok1 = sampleSnip -> workOnMe[0] -> NextOnQueue(0, value[0][0]);  // i is the first sample name to compare, 0 is the 1st allele from that sample name
+		// ok2 = sampleSnip -> workOnMe[0] -> NextOnQueue(1, value[0][1]);  // i is the first sample name to compare, 1 is the 2nd allele from that sample name
+		// ok3 = sampleSnip -> workOnMe[1] -> NextOnQueue(0, value[1][0]);  // j is the second sample name to compare, 0 is the 1st allele from that sample name
+		// ok4 = sampleSnip -> workOnMe[1] -> NextOnQueue(1, value[1][1]);
+	// }
+
+	mismatchedSnpsPerc = static_cast<float>(mismatchedSnps) / static_cast<float>(TotalSnps) * 100.0;
+	mismatchBothHomsPerc = static_cast<float>(mismatchBothHoms) / static_cast<float>(TotalSnps) * 100.0;
+	mismatchHetHomOverlapPerc = static_cast<float>(mismatchHetHomOverlap) / static_cast<float>(TotalSnps) * 100.0;
+	mismatchHetHomNonOverlapPerc = static_cast<float>(mismatchHetHomNonOverlap) / static_cast<float>(TotalSnps) * 100.0;
+	mismatchHetHetPerc = static_cast<float>(mismatchHetHet) / static_cast<float>(TotalSnps) * 100.0;
+
+    // stringstream os;
+	// os << std::setprecision(2) << std::fixed;
+
+	string sample1, sample2, SnpsPerc, BothHomsPerc, HomLapPerc, HetHomNonLapPerc, HetHetPerc;
+
+	sampleSnip -> workOnMe[0] -> CopyName(&sample1);
+	sampleSnip -> workOnMe[1] -> CopyName(&sample2);
+
+	stringstream temp22;
+
+	temp22 << std::setprecision(2) << std::fixed << mismatchedSnpsPerc << "%";
+	SnpsPerc = temp22.str();
+	temp22.str("");
+
+	temp22 << std::setprecision(2) << std::fixed << mismatchBothHomsPerc << "%";
+	BothHomsPerc = temp22.str();
+	temp22.str("");
+
+	temp22 << std::setprecision(2) << std::fixed << mismatchHetHomOverlapPerc << "%";
+	HomLapPerc = temp22.str();
+	temp22.str("");
+
+	temp22 << std::setprecision(2) << std::fixed << mismatchHetHomNonOverlapPerc << "%";
+	HetHomNonLapPerc = temp22.str();
+	temp22.str("");
+
+	temp22 << std::setprecision(2) << std::fixed << mismatchHetHetPerc << "%";
+	HetHetPerc = temp22.str();
+	temp22.str("");
+
+	temp22 << sample1 << "," << sample2 << "," << TotalSnps
+				<< "," << mismatchedSnps << "," << SnpsPerc << "," << mismatchBothHoms << ","
+				<< BothHomsPerc << "," << mismatchHetHomOverlap << "," << HomLapPerc << ","
+				<< mismatchHetHomNonOverlap << "," << HetHomNonLapPerc << ","
+				<< mismatchHetHet << "," << HetHetPerc << endl;
+
+    sampleSnip -> output = temp22.str();
+    // sampleSnip -> output = "test";
+    return NULL;
+}
+
+
 // -------------------------- v Main Starts Here v --------------------- //
 
 int main(int argc, char **argv) {  //get arguments from command line, i.e., yourexec filename
@@ -298,7 +399,7 @@ int main(int argc, char **argv) {  //get arguments from command line, i.e., your
 
             AllSamples.push_back(new Sample);
             loadme[i] -> ReadInThisSample = AllSamples[i];
-            loadme[i] -> delim = delim;
+            loadme[i] -> delim = &delim;
 
 			loadme[i] -> ReadInLine[0].str(row);
             getline(input_file, row);
@@ -307,180 +408,70 @@ int main(int argc, char **argv) {  //get arguments from command line, i.e., your
             pthread_create( &tempTid, NULL, ReadInSampleFunc, loadme[i] );
             tid.push_back(tempTid);
 
-            // sleep(1000);
-            // cout << "press enter for main to continue" << endl;
-            // getchar();
-
-			// getline(line1, Token, delim);
-			// if (i % 2 == 0) {
-			// 	AllSamples.push_back(new Sample);
-			// 	AllSamples[idiv2]->MakeTitle(Token);
-			// 	// string temp1;                        // Debugging to see that names ae loading correctly
-			// 	// AllSamples[idiv2].CopyName(&temp1);
-			// 	// std::cout << temp1 << endl;
-			// }
-
-			// while (getline(line1, Token, delim)) {
-			// 	// cout << Token << endl; // for debugging
-			// 	if (Token[0] == '-' || isdigit(Token[0])) {
-			// 		int mynumber = stoi(Token);
-			// 		AllSamples[idiv2]->AddToAllele((i % 2), mynumber);  // to add to the correct array of allele queues
-			// 		// int temp;
-			// 		// AllSamples[idiv2]->firstOnQueue((i%2), temp);   // Debugging code
-			// 		// std::cout << temp << endl;
-			// 	}
-			// }
-
 			i++;
-			// idiv2 = i / 2;
-			// std::cout << idiv2 << " | " << i % 2 << endl;
 		}
 	}
-	// bool ok22; //debugging printout
-	// int temp73;
-	// ok22 = AllSamples[0]->firstOnQueue(0, temp73);
-
-	// while (ok22) {
-	//        std::cout << temp73 << " | ";
-	//     ok22 = AllSamples[0]->NextOnQueue(0, temp73);
-
-	// }
-	// std::cout << endl ;
 
 	input_file.close();
 
 	SampleCount = i;
-	int totalTest = (SampleCount * (SampleCount - 1)) / 2;
-	//std::cout << SampleCount << endl;
-	i = 0;
-	float k = 0;
+	// int totalTest = (SampleCount * (SampleCount - 1)) / 2;
+
     // ----------      join here
 
-
+    for( i=0; i < tid.size(); ++i){
+        pthread_join( tid[i], NULL);                // Wait for any loading threads still waiting to complete before continuing
+    }
 
     // Write compare and write string stream here
 
+	tid.resize(0);
+    vector<snipItStruct *> twoSamples;
+    i = 0;
+	int k = 0;
+    while (i < SampleCount - 1) {
+		for (j = i + 1; j < SampleCount; ++j) {
 
+            twoSamples.push_back(new snipItStruct);
+            twoSamples[k] -> lowindex = &lowindex;
+            twoSamples[k] -> highindex = &highindex;
+            twoSamples[k] -> workOnMe[0] = AllSamples[i];
+            twoSamples[k] -> workOnMe[1] = AllSamples[j];
+
+            pthread_create( &tempTid, NULL, snipAndString, twoSamples[k] );
+            tid.push_back(tempTid);
+            ++k;
+            // pthread_join(tempTid, NULL);
+        }
+        // cout << "Do I make it here" << endl;
+        ++i;
+    }
 
     // write stirng stream to file here
+    argv[1][strlen(argv[1])-4] = '\0';
 	ofstream output_file(strcat(argv[1], "Snipped.csv"));
 
 	if (output_file.is_open()) {
 		output_file << "sample1,sample2,total SNPs,mismatched SNPs,mismatch %,Hom Hom (ex: 00 11),Hom Homs %,";
 		output_file << "Het Hom Lap (ex: 00 01),Het Hom Lap %,Het Hom non Lap (ex 00 12),Het Hom non Lap %,Het Het (ex: 01 12 or 01 23),Het Het %\n";
 
-		int TotalSnps, mismatchedSnps, mismatchBothHoms, mismatchHetHomOverlap, mismatchHetHomNonOverlap, mismatchHetHet;
-		float mismatchedSnpsPerc, mismatchBothHomsPerc, mismatchHetHomOverlapPerc, mismatchHetHomNonOverlapPerc, mismatchHetHetPerc;
-
 		std::cout << "File is loaded, output file is being written to: " << endl
 				  << argv[1] << endl
 				  << endl;
+    // Join here before writing to file
 
-		while (i < SampleCount - 1) {
-			for (j = i + 1; j < SampleCount; j++) {
+        for( i=0; i < tid.size(); ++i){
+            // pthread_join( tid[i], NULL);                // Wait for any loading threads still waiting to complete before continuing
+            output_file << twoSamples[i] -> output;
+        }
 
-                // Functionise this for THreading
-
-				TotalSnps = 0;
-				mismatchedSnps = 0;
-				mismatchBothHoms = 0;
-				mismatchHetHomOverlap = 0;
-				mismatchHetHomNonOverlap = 0;
-				mismatchHetHet = 0;
-
-				int value[2][2];  // Load an array with the sample values for comparison
-				bool ok1, ok2, ok3, ok4;
-				ok1 = AllSamples[i]->firstOnQueue(0, value[0][0]);  // i is the first sample name to compare, 0 is the 1st allele from that sample name
-				ok2 = AllSamples[i]->firstOnQueue(1, value[0][1]);  // i is the first sample name to compare, 1 is the 2nd allele from that sample name
-				ok3 = AllSamples[j]->firstOnQueue(0, value[1][0]);  // j is the second sample name to compare, 0 is the 1st allele from that sample name
-				ok4 = AllSamples[j]->firstOnQueue(1, value[1][1]);  // j is the second sample name to compare, 1 is the 2nd allele from that sample name
-
-				while (ok1 && ok2 && ok3 && ok4) {
-					sortSnips(value);
-
-					if (CheckTotal(value, lowindex, highindex)) {
-						TotalSnps++;
-						if (CheckMisMatch(value)) {
-							mismatchedSnps++;
-							if (CheckHomMisMatch(value)) {
-								mismatchBothHoms++;
-							} else if (CheckHetHomOverlapMismatch(value)) {
-								mismatchHetHomOverlap++;
-							} else if (CheckHetHomNonOverlapMismatch(value)) {
-								mismatchHetHomNonOverlap++;
-							} else if (CheckHetHetMisMatch(value)) {
-								mismatchHetHet++;
-							}
-						}
-					}
-
-					// std::cout << value[0][0] << value[0][1] << value[1][0] << value[1][1] << endl; // for debugging
-					ok1 = AllSamples[i]->NextOnQueue(0, value[0][0]);  // i is the first sample name to compare, 0 is the 1st allele from that sample name
-					ok2 = AllSamples[i]->NextOnQueue(1, value[0][1]);  // i is the first sample name to compare, 1 is the 2nd allele from that sample name
-					ok3 = AllSamples[j]->NextOnQueue(0, value[1][0]);  // j is the second sample name to compare, 0 is the 1st allele from that sample name
-					ok4 = AllSamples[j]->NextOnQueue(1, value[1][1]);
-				}
-
-				mismatchedSnpsPerc = static_cast<float>(mismatchedSnps) / static_cast<float>(TotalSnps) * 100.0;
-				mismatchBothHomsPerc = static_cast<float>(mismatchBothHoms) / static_cast<float>(TotalSnps) * 100.0;
-				mismatchHetHomOverlapPerc = static_cast<float>(mismatchHetHomOverlap) / static_cast<float>(TotalSnps) * 100.0;
-				mismatchHetHomNonOverlapPerc = static_cast<float>(mismatchHetHomNonOverlap) / static_cast<float>(TotalSnps) * 100.0;
-				mismatchHetHetPerc = static_cast<float>(mismatchHetHet) / static_cast<float>(TotalSnps) * 100.0;
-
-				std::cout << std::setprecision(2) << std::fixed;
-
-				string sample1, sample2, SnpsPerc, BothHomsPerc, HomLapPerc, HetHomNonLapPerc, HetHetPerc;
-
-				AllSamples[i]->CopyName(&sample1);
-				AllSamples[j]->CopyName(&sample2);
-
-				stringstream temp22;
-
-				temp22 << std::setprecision(2) << std::fixed << mismatchedSnpsPerc << "%";
-				SnpsPerc = temp22.str();
-				temp22.str("");
-
-				temp22 << std::setprecision(2) << std::fixed << mismatchBothHomsPerc << "%";
-				BothHomsPerc = temp22.str();
-				temp22.str("");
-
-				temp22 << std::setprecision(2) << std::fixed << mismatchHetHomOverlapPerc << "%";
-				HomLapPerc = temp22.str();
-				temp22.str("");
-
-				temp22 << std::setprecision(2) << std::fixed << mismatchHetHomNonOverlapPerc << "%";
-				HetHomNonLapPerc = temp22.str();
-				temp22.str("");
-
-				temp22 << std::setprecision(2) << std::fixed << mismatchHetHetPerc << "%";
-				HetHetPerc = temp22.str();
-				// temp22.str("");
-
-				// std::cout << sample1 << " vs " << sample2 << endl; //for debugging
-				output_file << sample1 << "," << sample2 << "," << TotalSnps
-							<< "," << mismatchedSnps << "," << SnpsPerc << "," << mismatchBothHoms << ","
-							<< BothHomsPerc << "," << mismatchHetHomOverlap << "," << HomLapPerc << ","
-							<< mismatchHetHomNonOverlap << "," << HetHomNonLapPerc << ","
-							<< mismatchHetHet << "," << HetHetPerc << endl;
-
-				if (argc == 3) {
-					k++;
-					stringstream output, leftsample2;
-					leftsample2 << left << setw(15) << sample2;
-					string opt, ls2;
-					ls2 = leftsample2.str();
-					output << setw(8) << setprecision(4) << k / static_cast<float>(totalTest) * 100.0 << "% records processed so far, sample pair: " << setw(15) << sample1 << " vs " << ls2 << " do not open " << argv[1] << " until completed." << '\r';
-					opt = output.str();
-					leftsample2.str("");
-					output.str();
-
-					cout << opt << '\r';
-				}
-			}
-			i++;
-		}
-	}
 	output_file.close();
+
+        for ( i=0; i < k; ++i){
+            delete twoSamples[i];
+        }
+
+	}
 	for (vector<Sample *>::iterator it = AllSamples.begin(); it != AllSamples.end(); it++) {
 		delete (*it);
 	}
@@ -489,7 +480,7 @@ int main(int argc, char **argv) {  //get arguments from command line, i.e., your
 			  << endl
 			  << "Processing Complete, you may now use " << argv[1] << endl
 			  << endl
-			  << "Thanks for using SnipIt.SnipitGood feel free to donate " << endl
+			  << "Thanks for using SnipIt feel free to donate " << endl
 			  << "or request other features or programs at www.SierraAlpha.co.nz" << endl
 			  << endl;
 }
